599c658a350c75bf46b0095e9265e49bd3d60a34
buruhsd <buruhsd@outlook.co.id>
2025-04-21 14:51:31 +0700

    update text

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World hmmmm!';
+    return 'perubahan terbaru, harusnya cepat!';
   }
 }
